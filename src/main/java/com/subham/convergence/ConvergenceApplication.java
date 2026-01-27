e29f2861fc8c0e72109fb1621907e9aa3c0d2b75
subhamagarrwal <130570909+subhamagarrwal@users.noreply.github.com>
2026-01-27 16:47:19 +0530

    initial commit

---
+++ src/main/java/com/subham/convergence/ConvergenceApplication.java
@@ -0,0 +1,13 @@
+package com.subham.convergence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvergenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvergenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subham/convergence/ConvergenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subham.convergence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvergenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
